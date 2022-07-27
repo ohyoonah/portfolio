9b39ddf05973299644ae6739bd81891845675007
ohyoonah <dhdbsdk3@naver.com>
2022-07-28 06:30:57 +0900

    home 화면에서는 최상위 버튼 숨기기

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // 헤더 스크롤 시 배경색 투명 --> 흰색으로 변경
 const header = document.querySelector('header');
+const up = document.getElementById('up');
 // getBoundingClientRect().height로 해당 DOM의 높이를 변수로 잡음
 const headerHeight = header.getBoundingClientRect().height;
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > headerHeight) {
     header.setAttribute("style", "background: white;");
+    up.setAttribute("style", "display: block");
   } else {
     header.setAttribute("style", "background: transparent;");
+    up.setAttribute("style", "display: none");
   }
 });
 
